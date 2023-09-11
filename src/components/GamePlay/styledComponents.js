1ef3538c8500608fd2632ddf8c7d3a26aff584d1
Bhavana <bhavanapoloju60@gmail.com>
2023-09-11 06:07:45 +0000

    rockPaperScissor

---
+++ src/components/GamePlay/styledComponents.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const GamePlayMainContainer = styled.div`
+  display: flex;
+`
+
+export const GameRulesView = styled.div`
+  align-self: flex-end;
+`
+export const PopUpView = styled.div`
+  display: flex;
+`
+
+export const PopUpImage = styled.div`
+  margin-top: auto;
+`
---
+++ src/components/GameResults/styledComponents.js
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const GameContainer = styled.div`
+  display: flex;
+`
+
+export const GameButton = styled.div`
+  display: flex;
+`
+
+export const GameImage = styled.img`
+  width: 150px;
+`
+export const ResultImageContainer = styled.div`
+  display: flex;
+`
+export const ResultName = styled.p`
+  color: white;
+`
+export const ResultText = styled.p`
+  display: flex;
+`
+
+export const ResultButton = styled.button`
+  display: flex;
+`
